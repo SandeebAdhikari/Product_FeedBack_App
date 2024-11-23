670a7117dc1f1f9bda996a8982b854e9b4c7edf5
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-23 10:17:33 -0500

    assets and tailwind.config.ts and sidebar updated

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SideBar = () => {
-  return <div className="w-[255px] h-[529px] border rounded-[10px]"></div>;
+  return (
+    <div className="w-[255px] h-[529px] border rounded-[10px]">
+      <div className="w-full h-[137px] "></div>
+    </div>
+  );
 };
 
 export default SideBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,37 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primaryPurple: "#AD1FEA",
+        primaryBlue: "#4661E6",
+        white: "#FFFFFF",
+        lightBlue: "#F2F4FF",
+        veryLightBlue: "#F7F8FD",
+        darkBlue: "#3A4374",
+        grayBlue: "#647196",
+        coral: "#F49F85",
+        skyBlue: "#62BCFA",
+      },
+      textColor: {
+        silver: "#F7F8FD",
+        lightYellow: "#F49F85",
+      },
+      backgroundColor: {
+        lighterPeach: "#F7F8FD",
+        peach: "#F49F85",
+        semiDarkNavy: "#3A4374",
+        darkNavy: "#647196",
+      },
+      fontFamily: {
+        jost: ["Jost", "sans-serif"],
+      },
+      fontSize: {
+        h1: ["24px", { lineHeight: "35px", letterSpacing: "-0.33px" }],
+        h2: ["20px", { lineHeight: "29px", letterSpacing: "-0.25px" }],
+        h3: ["18px", { lineHeight: "26px", letterSpacing: "-0.25px" }],
+        h4: ["14px", { lineHeight: "20px", letterSpacing: "-0.2px" }],
+        body1: ["16px", { lineHeight: "23px" }],
+        body2: ["15px", { lineHeight: "22px" }],
+        body3: ["13px", { lineHeight: "19px" }],
       },
     },
   },
